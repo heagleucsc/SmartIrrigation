f4df37fa80ac1bf5740b943718dc7c03e1a49c09
Danny <dmfriberg@gmail.com>
2017-07-16 14:39:03 -0700

    API test
    Barebones with authentication functionality

---
+++ src/pages/login/login.ts
@@ -0,0 +1,57 @@
+import { Component } from '@angular/core';
+
+import { NavController, NavParams } from 'ionic-angular';
+
+import { HelloIonicPage } from '../hello-ionic/hello-ionic';
+
+import CryptoJS from 'crypto-js'
+
+import * as $ from 'jquery';
+
+
+@Component({
+  selector: 'page-login',
+  templateUrl: 'login.html'
+})
+export class LoginPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  login(event) {
+    let myUsername = (<HTMLInputElement>document.getElementById("username")).value;
+    let myPassword = (<HTMLInputElement>document.getElementById("password")).value;
+    let hashedPass = CryptoJS.MD5(myPassword).toString();
+    console.log(hashedPass);
+    if(!myUsername || !myPassword) {
+      console.log("error, fields are empty");
+      return;
+    }
+    if(typeof(Storage) !== "undefined"){
+      let form = this;
+      sessionStorage.setItem("username", myUsername);
+      myPassword = hashedPass;
+      console.log(myUsername+myPassword);
+      $.ajax({
+        type: "POST",
+        //url: "/api/users/login",
+        url: "https://slugsense.herokuapp.com/api/users/login",
+        data: {
+          username: myUsername,
+          password: myPassword
+        },
+        success: function(data){
+          console.log(data);
+          localStorage.setItem("token", data.api_token);
+          form.navCtrl.push(HelloIonicPage);
+        },
+        error: function(err) {
+          console.log(err);
+          console.log("Invalid Credentials");
+        }
+
+      });
+    }
+
+  }
+};
