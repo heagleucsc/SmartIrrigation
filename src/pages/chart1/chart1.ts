212fced5669998597bc1ce24aa8c44351c6a7e6b
heagleucsc <heagle@ucsc.edu>
2017-07-17 20:37:53 -0700

    all graph files

---
+++ src/pages/chart1/chart1.ts
@@ -0,0 +1,113 @@
+import {Component, ViewChild} from '@angular/core';
+import {EChartsComponent} from "../../components/echart-component";
+
+@Component({
+ selector: 'page-chart1',
+ templateUrl: 'chart1.html'
+})
+export class Chart1Page {
+
+ @ViewChild(EChartsComponent)
+ chart;
+
+ ionViewDidEnter() {
+   this.chart.resize();
+ }
+
+ option = {
+   backgroundColor: ['#394058'],
+   
+   title: {
+        text: 'Sensor 1a',
+        textStyle: {
+            fontWeight: 'normal',
+            fontSize: 16,
+            color: '#F1F1F3'
+        },
+        left: '6%'
+    },
+	tooltip: {
+        trigger: 'axis',
+        axisPointer: {
+            lineStyle: {
+                color: '#57617B'
+            }
+        }
+    },
+	legend: {
+        icon: 'rect',
+        itemWidth: 14,
+        itemHeight: 5,
+        itemGap: 13,
+        data: ['Humidity'],
+        right: '4%',
+        textStyle: {
+            fontSize: 12,
+            color: '#F1F1F3'
+        }
+    },
+   color: ['#3398DB'],
+   grid: {
+     left: '3%',
+     right: '4%',
+     bottom: '3%',
+     containLabel: true
+   },
+   xAxis: [{
+        type: 'category',
+        boundaryGap: false,
+        axisLine: {
+            lineStyle: {
+                color: '#57617B'
+            }
+        },
+        data: ['13:00', '13:05', '13:10', '13:15', '13:20', '13:25', '13:30', '13:35', '13:40', '13:45', '13:50', '13:55']
+    }],    
+   yAxis: [{
+        type: 'value',
+        name: 'Humidity（%）',
+        axisTick: {
+            show: false
+        },
+        axisLine: {
+            lineStyle: {
+                color: '#57617B'
+            }
+        },
+        axisLabel: {
+            margin: 10,
+            textStyle: {
+                fontSize: 14
+            }
+        },
+        splitLine: {
+            lineStyle: {
+                color: '#57617B'
+            }
+        }
+    }],
+   series: [
+     {
+       name: 'Humidity',
+       type: 'line',
+       smooth: true,
+        symbol: 'circle',
+        symbolSize: 5,
+        showSymbol: false,
+        lineStyle: {
+            normal: {
+                width: 1
+            }
+        },
+		itemStyle: {
+            normal: {
+                color: 'rgb(137,189,27)',
+                borderColor: 'rgba(137,189,2,0.27)',
+                borderWidth: 12
+            }
+        },
+       data: [70, 55, 60, 45, 71, 71, 72, 69, 77, 52, 70, 65]
+     }
+   ]
+ };
+}
