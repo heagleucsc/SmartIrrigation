28441d92ab9a15d055f98fc2c6947c47ffab2c0f
Yuqiao Jiang <yujiang@ucsc.edu>
2017-07-21 06:47:59 -0700

    Passed user data from login to home without setting localstorage, made updateNodeIds() obsolete, function updateInfo() updates user info, node list, node its and latest node readings with just two api calls.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,11 @@
 import {Component, ViewChild} from '@angular/core';
-import { timeBoxedData } from './nodeData';
+import {EChartsComponent} from "../../components/echart-component";
+import { NavController, NavParams } from 'ionic-angular';
+import * as nodeData from '../home/nodeData';
+import {timeBoxedData } from './nodeData';
+
+import * as $ from 'jquery';
+
 import { page_data } from './page_data';
 
 @Component({
@@ -7,74 +13,256 @@ import { page_data } from './page_data';
   templateUrl: 'home.html'
 })
 
+
+
 export class HomePage {
-  data: page_data
+  /* User holds json data */
+  data: page_data;
+
+  token = null;
+  base_url = "https://slugsense.herokuapp.com";
+
+  // Unused for now
+  nodes = {};
+  // 
+  user = null;
   nodeIds: number[] = [];
   nodeIndex = 0;
+  //currentNid;
+
+  message = "Hello"
   //refrest time (in milliseconds, change according to preference, current set at 10 seconds for testing)
   refresh_time = 10000;
+
+
+
+  // Graph options //
+  // Graph stuff should probably be moved to its own class later
   mode_day = true;
 
-  constructor() {
-    let nids = localStorage.getItem("nids");
-    if (!nids) this.updateNodeIds;
-    else this.nodeIds = JSON.parse(nids);
-    this.data = new page_data(this.currentNid());
+  constructor(navCtrl: NavController, navParams: NavParams) {
+    this.token = localStorage.getItem("token");
+    if (this.token == null){
+      console.log("token lost");
+    }
+    this.user = navParams.get('user');
+    this.compileNodeList(this.user.nodes);
+    this.getLatestAll().done(this.updateLatestAll);
+    this.data = new page_data(this.currNid());
+    console.log(this.user);
+    console.log(this.nodes);
+    console.log(this.nodeIds);
+    //this.currentNid = this.nodeIds[0];
 
-    //runs the first time
-    this.updateInfo();
     //runs after intervals
     setInterval(this.updateInfo.bind(this), this.refresh_time);
   }
 
 
-  updateInfo()  {
-    console.log("updating info")
-    this.data.updateData();
-    // this.updateButtons()
-    // this.updateGraph()
-    this.updateNodeIds()
-
-    this.data.logLatest();
+  /*
+    Functions called via events on the home page
+  */
+  //---------------------------------------------------------------------//
+  log24hrData(event){
+    //let nid = (<HTMLInputElement>document.getElementById("nid")).value;
+    //let timestamp = (<HTMLInputElement>document.getElementById("timestamp")).value;
+    let timestamp = $('#timestamp').val();
+    this.get24hrData(this.currNid(),timestamp).done(this.handleData);
   }
-
-
-  // Events from interaction with components // 
+  logLatestAll(event){
+    this.getLatestAll().done(this.consoleLog);
+  }
+  logLatestNid(event){
+    //let nid = (<HTMLInputElement>document.getElementById("nid")).value;
+    this.getLatestNode(this.currNid()).done(this.consoleLog);
+  }
+  // updateNodeIds(){
+  //   this.getLatestAll().done(this._updateNodeIds);
+  // }
+  test(){
+    //let nid = (<HTMLInputElement>document.getElementById("nid")).value;
+    //let timestamp = (<HTMLInputElement>document.getElementById("timestamp")).value;
+    let timestamp = $('#timestamp').val();
+    this.get24hrData(this.currNid(), timestamp).done(this.handleData);
+  }
   toggleDailyWeekly(){
     this.mode_day = !this.mode_day;
     // this.updateGraph() // -- implement in future
-  };
+  }
   changeNid(){
     this.nodeIndex = (this.nodeIndex + 1) % this.nodeIds.length;
-    this.data.changeNid(this.currentNid());
-  };
-  printNodeIds(){
-    console.log(this.nodeIds);
   }
 
+  getLatest(field: string, nid: number): number {
+    //return -1;
+    let node = this.nodes[nid];
+    if (!node) return -4;
+    console.log(node);
+    if (!("latest" in node)) return -3;
+    let latest = node.latest;
+    if (!latest) return -2;
+    console.log(latest);
+    if (field in latest)
+      return latest.field;
+    return -1;
+  }
+
+  /* Pulls data from all nodes */
+  updateInfo() {
+    this.getUserInfo().done(function(data) {
+      this.user = data;
+      this.compileNodeList(this.user.nodes);
+      //console.log(this.user);
+    });
+    this.getLatestAll().done(this.updateLatestAll);
+  }
+  //---------------------------------------------------------------------//
+
+
+/*
+      HOW TO USE AJAX CALLS
+
+    Ajax calls are asynchronous so they cannot change values of variables
+    outside of its scope, including fields of the HomePage class itself.
+
+    To use the data:
+    *nameOfAjaxCall*(*params*).done(*nameOfDataHandlingFunction*);
+
+    There is no need to specify the parameters of the data handle function,
+    as the data from the ajax call will be passed automatically as the input.
+    The done() function is called once the server responds with the data and
+    the specified data handling function is called.
+
+    Heres an example that simply prints any data from the call to the console:
+
+      this.getLatestNode(51).done(this.consoleLog);
+
+    Both involved functions are defined below.
 
 
 
+*/
 
-  // Utility functions //
-  currentNid(): number {
-    return this.nodeIds[this.nodeIndex];
+
+  /*
+    Lists of ajax calls that returns data
+  */
+  //---------------------------------------------------------------------//
+  get24hrData(nid, _timestamp){
+    if (_timestamp){
+      return $.ajax({
+        type: "POST",
+        dataType: "json",
+        url: this.base_url+"/api/nodes/prev_24h/"+nid.toString(),
+        data: {api_token: this.token, timestamp: _timestamp }
+      });
+    }
+    return $.ajax({
+      type: "POST",
+      dataType: "json",
+      url: this.base_url+"/api/nodes/prev_24h/"+nid.toString(),
+      data: {api_token: this.token}
+    });
   };
 
-  // I should really instead pass this context
-  // and handle the update in page_data.ts
-
-  // but I think node ids should be dealt with in home.ts
-  // since page_data should be specific to a single node
-  updateNodeIds() {
-    this.data.getLatestAll().done(function(data)
-    {
-      let nids: number[] = [];
-      for (let node of data){
-        if ("nodeId" in node)
-        nids.push(node["nodeId"]);
-      }
-      this.nodeIds = nids;
+  getLatestNode(nid){
+    return $.ajax({
+      type: "POST",
+      dataType: "json",
+      url: this.base_url+"/api/nodes/"+nid.toString()+"/latest_reading",
+      data: { api_token: this.token}
     });
   }
+
+  getLatestAll() {
+    return $.ajax({
+      context: this,
+      url: this.base_url + "/api/nodes/latest_readings/all",
+      type:"POST",
+      data: {api_token: this.token},
+    })
+  }
+
+  getNodeReadings(nid){
+    return $.ajax({
+      type: "GET",
+      dataType: "json",
+      url: this.base_url+"/api/nodes/"+nid.toString(),
+      data: {} //api_token: this.token}
+    });
+  }
+
+  getUserInfo() {
+    return $.ajax({
+      context: this,
+      url: this.base_url + "/api/users/getuser",
+      type:"POST",
+      data: {api_token: this.token},
+    });
+  }
+  //---------------------------------------------------------------------//
+
+
+  /*
+  Data handling functions
+
+  Write functions that handle data recieved from ajax calls here
+  */
+  //---------------------------------------------------------------------//
+  consoleLog(data){
+    console.log(data);
+  };
+
+  // _updateNodeIds(data: Object[]){
+  //   let nids: number[] = [];
+  //   for (let node of data){
+  //     //console.log(node);
+  //     if ("nodeId" in node)
+  //       nids.push(node["nodeId"]);
+  //   }
+  //   this.nodeIds = nids;
+  //   //localStorage.setItem("nids", JSON.stringify(nids))
+  // };
+
+  //---------------------------------------------------------------------//
+
+
+  /*
+  Utility functions
+  */
+  //---------------------------------------------------------------------//
+  printNodeIds(){
+    console.log(this.nodeIds);
+  };
+
+  handleData(data) {
+    //console.log(data);
+    let box : timeBoxedData = new timeBoxedData(data, 24);
+    console.log(box);
+    console.log(box.getDataAsDict());
+  }
+
+  currNid(): number {
+    return this.nodeIds[this.nodeIndex];
+  }
+  compileNodeList(data){
+    this.nodeIds = [];
+    for (let node of data){
+      this.nodes[node.id] = node;
+      this.nodeIds.push(<number>(node.id));
+    }  
+  }
+
+  updateLatestAll(data) {
+    for(let reading of data) {
+        this.nodes[reading.nodeId]["latest"] = reading; 
+     }
+  }
+
+
+
+
+
+ //end graph
+
 }
--- src/pages/home/page_data.ts
+++ src/pages/home/page_data.ts
@@ -2,228 +2,54 @@ import {timeBoxedData } from './nodeData';
 
 import * as $ from 'jquery';
 
-
-// page_data class
-/*
-  For the sake of legibility, the code that handles data
-  has been migrated to this class. Therefore, all actions
-  involving the display and retrieving of data should be
-  done through this class. Home.ts will instead deal
-  primarily with interactions with html elements and 
-  automated features.
-
-  Furthermore, having a seperate class for data handling
-  allows for an simpler time handling null data that may
-  result from an incompleted ajax call.
-
-  
-  This class will simplify several actions:
-    1. When changing the node to display,
-      simply call changeNid(), ie after 
-      another node is selected in menu
-
-    2. Initializing data on main page will
-      be a one liner constructor call
-
-
-
-*/
 export class page_data{
   _nid;
   token;
   _data: timeBoxedData; 
-  _latest;
   initialized: false;
   base_url = "https://slugsense.herokuapp.com";
-  // visual params //
-  // .. 
-  // ..
 
   constructor(nid: number){
     this._nid = nid;
     this.token = localStorage.getItem("token");
-
-    // Using result from login.ts ensures that the
-    // initial display of data on the buttons are accurate
-
-    // Not implemented yet - work to be done in login.ts
-    this._latest = localStorage.getItem("latest");
-
     if (this.token == null){
       console.log("token lost");
     }
     this.update24hrData();
   }
 
-  // Public access function to pull data
-  // for displaying on the button
-  //
-  // Babandeep requested that buttons display
-  // the latest none null node, so this data
-  // should return data from latest_reading call
-  // rather than from that of the prev_24 call
-
-  public getLatest(field: string){
-    return this._latest;
-    /*
+  getLatest(field: string){
     if (!this._data) return -1;
     let latest = this._data.getLatestData();
     if ( !(field in latest) ) return -1;
     return latest[field];
-    */
+  }
+
+  update24hrData(){
+    this.get24hrData().done(function(data){
+      this._data = new timeBoxedData(data, 24);
+    });
   }
 
 
-  public updateData(){
-    this.update24hrData();
-    this.updateLatest();
-  }
-
-  public update24hrData(){
-    this.get24hrData(this._nid).
-      fail(function(err){
-        console.log(err);
-        console.log("Node id invalid or server timeout");
-      }).
-      done(function(data){
-        this._data = new timeBoxedData(data, 24);
-      });
-    
-    // updateGraphOptions() // 
-  };
-
-  public updateLatest(){
-    this.getLatestNode(this._nid).
-      fail(function(err){
-        console.log(err);
-        console.log("Error in updating latest data");
-      }).
-      done(function(data){
-        this._latest = data;
-      });
-  }
-
-
-  public changeNid(nid){
-    this._nid = nid;
-    this.update24hrData();
-    this.updateLatest();
-    // updateGraphOptions() //
-  }
-
-
-  // For Heather //
-
-  // updateGraphOptions(params){
-
-  // }
-
-
-
-
-  // jQuery ajax calls //
-
-/*
-      HOW TO USE AJAX CALLS
-
-    Ajax calls are asynchronous so they cannot change values of variables
-    outside of its scope immediately. So, the data from the call should be
-    used within the scope of the call like so:
-
-    *nameOfAjaxCall*(*params*).done(*nameOfDataHandlingFunction*);
-
-    There is no need to specify the parameters of the data handle function,
-    as the data from the ajax call will be passed automatically as the input.
-    The done() function is called once the server responds with the data and
-    the specified data handling function is called.
-
-    Heres an example that simply prints any data from the call to the console:
-
-      this.getLatestNode(51).done(this.consoleLog);
-
-    Both involved functions are defined below.
-
-*/
-
-  private get24hrData(nid, _timestamp = null){
+  private get24hrData(_timestamp = null){
     if (_timestamp){
       return $.ajax({
         type: "POST",
         dataType: "json",
-        url: this.base_url+"/api/nodes/prev_24h/"+nid.toString(),
+        url: this.base_url+"/api/nodes/prev_24h/"+this._nid,
         data: {api_token: this.token, timestamp: _timestamp }
       });
-    };
+    }
     return $.ajax({
       type: "POST",
       dataType: "json",
-      url: this.base_url+"/api/nodes/prev_24h/"+nid.toString(),
+      url: this.base_url+"/api/nodes/prev_24h/"+this._nid,
       data: {api_token: this.token}
     });
   };
-  
-  private getLatestNode(nid){
-    return $.ajax({
-      type: "POST",
-      context: this,
-      dataType: "json",
-      url: this.base_url+"/api/nodes/"+nid.toString()+"/latest_reading",
-      data: { api_token: this.token}
-    });
-  };
 
-  // Unused 
-  private getUserInfo() {
-    return $.ajax({
-      context: this,
-      url: this.base_url + "/api/users/getuser",
-      type:"POST",
-      data: {api_token: this.token},
-    });
-  };
-
-  public getLatestAll() {
-    return $.ajax({
-      context: this,
-      url: this.base_url + "/api/nodes/latest_readings/all",
-      type:"POST",
-      data: {api_token: this.token},
-    })
-  };
-
-  
-
-  /* Utilities */
-
-  // Called from home to update list of node ids
-  public setNodeIds() {
-    this.getLatestAll().done(function(data)
-    {
-      let nids: number[] = [];
-      for (let node of data){
-        if ("nodeId" in node)
-        nids.push(node["nodeId"]);
-      }
-      localStorage.setItem("nids", JSON.stringify(nids));
-    });
-  }
-
-  // 
-  public getNodeIds(){
-    return this.getLatestAll();
-  }
-
-  public log24hr(){
-    console.log(this._data);
-  }
-
-  public logLatest(){
-    console.log(this._latest)
-  }
-  
 
 
 
 }
-
-
